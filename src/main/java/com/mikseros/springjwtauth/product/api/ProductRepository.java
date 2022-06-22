1fbe8dabed22b0369798ba39845156aadfaf2d6c
mikseros <mmadynski@gmail.com>
2022-06-22 21:10:32 +0200

    Product REST API -> repository

---
+++ src/main/java/com/mikseros/springjwtauth/product/api/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.mikseros.springjwtauth.product.api;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer>{
+
+}
