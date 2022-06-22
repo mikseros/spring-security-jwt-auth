2f9d344b79b0b2b7aa3c6e67bb5eef7589e3c4ac
mikseros <mmadynski@gmail.com>
2022-06-22 21:19:37 +0200

    Initial conf for security. Allowing public access.

---
+++ src/main/java/com/mikseros/springjwtauth/security/ApplicationSecurityConfig.java
@@ -0,0 +1,17 @@
+package com.mikseros.springjwtauth.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests().anyRequest().permitAll();
+	}
+}
