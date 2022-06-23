9040ea190606dd4f304a8afed0d6eeb0c08bae54
mikseros <mmadynski@gmail.com>
2022-06-23 17:49:59 +0200

    Test for User creation.

---
+++ src/test/java/com/mikseros/springjwtauth/user/UserRepositoryTests.java
@@ -0,0 +1,35 @@
+package com.mikseros.springjwtauth.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+
+	@Autowired
+	UserRepository repo;
+	
+	@Test
+	public void testCreateUser() {
+		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String rawPassword = "123456";
+		String encodedPassword = passwordEncoder.encode(rawPassword);
+		
+		User newUser = new User("xxx@xxx.com", encodedPassword);
+		
+		User savedUser = repo.save(newUser);
+		
+		assertThat(savedUser).isNotNull();
+		assertThat(savedUser.getId()).isGreaterThan(0);
+	}
+}
