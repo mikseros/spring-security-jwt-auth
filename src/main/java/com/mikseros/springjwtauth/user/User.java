7d904c3b6369ffc70391676f47f0a0228299d4a4
mikseros <mmadynski@gmail.com>
2022-06-23 17:26:53 +0200

    User Entity/Model

---
+++ src/main/java/com/mikseros/springjwtauth/user/User.java
@@ -0,0 +1,58 @@
+package com.mikseros.springjwtauth.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(nullable = false, unique = true, length = 50)
+	private String email;
+	
+	@Column(nullable = false, length = 64)
+	private String password;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public User() {
+		
+	}
+	
+	public User(String email, String password) {
+		super();
+		this.email = email;
+		this.password = password;
+	}
+	
+	
+}
