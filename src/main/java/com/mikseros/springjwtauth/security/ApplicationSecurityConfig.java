0e485168bd25e48de6248a8d091a5cc4c6353c77
mikseros <mmadynski@gmail.com>
2022-06-23 11:21:38 +0200

    WebSecurityConfigurerAdapter Deprecated Fixed

--- src/main/java/com/mikseros/springjwtauth/security/ApplicationSecurityConfig.java
+++ src/main/java/com/mikseros/springjwtauth/security/ApplicationSecurityConfig.java
@@ -1,17 +1,20 @@
 package com.mikseros.springjwtauth.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
-@EnableWebSecurity
-public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
+@Configuration
+public class ApplicationSecurityConfig {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.authorizeRequests().anyRequest().permitAll();
+		
+		return http.build();
 	}
 }
