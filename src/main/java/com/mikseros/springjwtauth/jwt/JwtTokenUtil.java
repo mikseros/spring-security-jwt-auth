7db4f5980c24031e779335dc6f00c55e44fe777e
mikseros <mmadynski@gmail.com>
2022-06-26 20:41:13 +0200

    JWT Token Validation

--- src/main/java/com/mikseros/springjwtauth/jwt/JwtTokenUtil.java
+++ src/main/java/com/mikseros/springjwtauth/jwt/JwtTokenUtil.java
@@ -2,17 +2,24 @@ package com.mikseros.springjwtauth.jwt;
 
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.mikseros.springjwtauth.user.User;
-
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 
 @Component
 public class JwtTokenUtil {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(JwtTokenUtil.class);
+	
 	private static final long EXPIRE_DURATION = 24 * 60 * 60 * 1000; // 24H
 	
 	@Value("${app.jwt.secret}")
@@ -26,5 +33,26 @@ public class JwtTokenUtil {
 				.setExpiration(new Date(System.currentTimeMillis() + EXPIRE_DURATION))
 				.signWith(SignatureAlgorithm.HS512, secretKey)
 				.compact();
+	}
+	
+	public boolean validateAccessToken(String token) {
+		try {
+			Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+			
+			return true;
+			
+		} catch (ExpiredJwtException ex) {
+			LOGGER.error("JWT expired", ex);
+		} catch (IllegalArgumentException ex) {
+			LOGGER.error("Token is null, empty or has only whitespaces.", ex);
+		} catch (MalformedJwtException ex) {
+			LOGGER.error("JWT is invalid", ex);
+		} catch (UnsupportedJwtException ex) {
+			LOGGER.error("JWT is not supported", ex);
+		} catch (SignatureException ex) {
+			LOGGER.error("Signature validation failed", ex);
+		}
+		
+		return false;
 	}
 }
